bf8710856b54642ad38d85dcfddd9f8f15d5d41b
Kate Mcguane <katemcguane@hotmail.com>
2024-07-11 14:50:16 +0000

    Create alert_santa method

--- student.py
+++ student.py
@@ -1,8 +1,8 @@
 from datetime import date, timedelta
 
-class Student:
-    """A Student class as a basic for method testing"""
 
+class Student:
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -11,7 +11,10 @@ class Student:
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
-
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+
+    def alert_santa(self):
+        self.naughty_list= True
