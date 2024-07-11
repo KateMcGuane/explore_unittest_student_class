ebd3878086339cffe1a0674177b0c868ec9d48ae
Kate Mcguane <katemcguane@hotmail.com>
2024-07-11 15:34:04 +0000

    Creat setUpClass & tearDownClass

--- test_student.py
+++ test_student.py
@@ -4,6 +4,16 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+
     def setUp(self):
         print("setUp")
         self.student = Student("John", "Doe")
@@ -17,14 +27,15 @@ class TestStudent(unittest.TestCase):
         print("test_full_name")
         self.assertEqual(self.student.full_name, "John Doe")
 
+
     def test_email(self):
         print("test_email")
         self.assertEqual(self.student.email, "john.doe@email.com")
 
+
     def test_alert_santa(self):
         print("test_alert_santa")
         self.student.alert_santa()
-
         self.assertTrue(self.student.naughty_list)
 
 
