53494f52dbdfe692c41974094ec9ae2427e575f6
Kate Mcguane <katemcguane@hotmail.com>
2024-07-11 14:27:34 +0000

    Create an instance of the Student class and assert that the full_name method returns the correctly formatted student name

--- test_student.py
+++ test_student.py
@@ -2,8 +2,13 @@ import unittest
 from student import Student
 
 
-class TestStudent(unittest.TestCase)
+class TestStudent(unittest.TestCase):
+
+    def test_full_name(self):
+        student = Student('John', 'Doe')
+
+        self.assertEqual(student.full_name, 'John Doe')
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
