83e7a1b76b3d540e3cbf2e15d6878eba1b62d877
Kate Mcguane <katemcguane@hotmail.com>
2024-07-13 09:49:10 +0000

    Create course_schedule method

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -30,3 +31,12 @@ class Student:
         self.end_date = self.end_date + timedelta(days=days)
         # can also shorten code to:
         #self.end_date += timedelta(days=days)
+
+
+    def course_schedule(self):
+        response = requests.get("https://company.com/course-schedule/{self.last_name}/{self.first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
